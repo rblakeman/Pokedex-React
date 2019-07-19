daf0cfc941efd8f48f6eedebc5c2c831ad8574e2
Ryan Blakeman <rblakeman31@gmail.com>
2019-07-18 23:10:02 -0700

    Updates

---
+++ precache-manifest.c277b69e05a28d1aa6ffd2f21c1100a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d354b18a3229210310fda69c14dad17",
+    "url": "/PokedexApp/index.html"
+  },
+  {
+    "revision": "b168cc774a3f9b24efa3",
+    "url": "/PokedexApp/static/css/main.68a98085.chunk.css"
+  },
+  {
+    "revision": "3afb802d1206b0553c89",
+    "url": "/PokedexApp/static/js/2.7cbed5b7.chunk.js"
+  },
+  {
+    "revision": "b168cc774a3f9b24efa3",
+    "url": "/PokedexApp/static/js/main.9b0c8780.chunk.js"
+  },
+  {
+    "revision": "0c87966f0d21d5b2d9e3",
+    "url": "/PokedexApp/static/js/runtime~main.02b56f86.js"
+  }
+]);
