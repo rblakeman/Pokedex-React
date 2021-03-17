e40de9a14ab1aef4a2763dd909210c404a57c2db
Ryan Blakeman <rblakeman31@gmail.com>
2021-03-16 19:14:40 -0700

    linting

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from 'react';
 
-import Input from './componenets/input'
-import Pokemon from './componenets/pokemon'
+import './App.css';
+import Input from './componenets/input';
+import Pokemon from './componenets/pokemon';
+
 const styles = {
-  container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  title: { margin: '20px', fontSize: '24px' },
-  inputs: { padding: '10px' }
-}
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    title: { margin: '20px', fontSize: '24px' },
+    inputs: { padding: '10px' }
+};
 
 const LOADING_STATE = {
-  loaded: false,
-  entry: '',
-  name: '',
-  sprite: '',
-  types: ['???'],
-  stats: [],
-  moves: [],
-  height: '',
-  weight: ''
-}
+    loaded: false,
+    entry: '',
+    name: '',
+    sprite: '',
+    types: ['???'],
+    stats: [],
+    moves: [],
+    height: '',
+    weight: ''
+};
 
 // #ee1515 red
 // #f00000 red
@@ -31,175 +32,170 @@ const LOADING_STATE = {
 // #f0f0f0 white
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.PokeAPI = this.PokeAPI.bind(this)
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this)
-    this.PokeAPIitem('4')
+    constructor(props) {
+        super(props);
+        this.PokeAPI = this.PokeAPI.bind(this);
+        this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+        this.PokeAPIitem('4');
 
-    this.state = {
-      ...LOADING_STATE,
-      windowWidth: 0,
-      windowHeight: 0,
-      shrink: false
+        this.state = {
+            ...LOADING_STATE,
+            windowWidth: 0,
+            windowHeight: 0,
+            shrink: false
+        };
     }
-  }
 
-  componentDidMount() {
-    this.updateWindowDimensions()
-    window.addEventListener('resize', this.updateWindowDimensions)
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateWindowDimensions)
-  }
-
-  updateWindowDimensions() {
-    if (window.innerHeight < 500) {
-      if (!this.state.shrink) this.setState({ shrink: true })
-    } else {
-      if (this.state.shrink) this.setState({ shrink: false })
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
     }
-    this.setState({
-      windowWidth: window.innerWidth,
-      windowHeight: window.innerHeight
-    })
-  }
 
-  PokeAPI(entry) {
-    this.setState({
-      ...LOADING_STATE
-    })
-    fetch(`https://pokeapi.co/api/v2/pokemon/${entry}/`)
-      .then((response) => {
-        if (!response.ok) {
-          let failedRespones = [
-            'You missed the Pokemon!',
-            'Darn! The Pokemon broke free!',
-            'Aww! It appeared to be caught!',
-            'Shoot! It was so close too!'
-          ]
-          console.log(
-            failedRespones[Math.floor(Math.random() * failedRespones.length)]
-          )
-          return false
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
+
+    updateWindowDimensions() {
+        if (window.innerHeight < 500) {
+            if (!this.state.shrink) this.setState({ shrink: true });
+        } else {
+            if (this.state.shrink) this.setState({ shrink: false });
         }
-        return response.json()
-      })
-      .then((data) => {
-        if (!data) return
-        // console.log(data)
-
-        let newTypes = []
-        data.types.forEach((e) => {
-          newTypes.push(e.type.name)
-        })
-
-        let newStats = []
-        data.stats.forEach((e) => {
-          newStats.push({
-            name: e.stat.name,
-            base_stat: e.base_stat,
-            effort: e.effort
-          })
-        })
-
-        let newMoves = []
-        data.moves.forEach((e, i) => {
-          newMoves.push({
-            name: e.move.name,
-            version_group_details: e.version_group_details
-          })
-        })
-
         this.setState({
-          name: data.name,
-          sprite: data.sprites.front_default,
-          entry: data.id,
-          types: newTypes,
-          stats: newStats,
-          moves: newMoves,
-          height: data.height,
-          weight: data.weight,
-          loaded: true
-        })
-        console.log(
-          `All right! ${data.name.charAt(0).toUpperCase() +
+            windowWidth: window.innerWidth,
+            windowHeight: window.innerHeight
+        });
+    }
+
+    PokeAPI(entry) {
+        this.setState({
+            ...LOADING_STATE
+        });
+        fetch(`https://pokeapi.co/api/v2/pokemon/${entry}/`)
+            .then((response) => {
+                if (!response.ok) {
+                    let failedRespones = [
+                        'You missed the Pokemon!',
+                        'Darn! The Pokemon broke free!',
+                        'Aww! It appeared to be caught!',
+                        'Shoot! It was so close too!'
+                    ];
+                    console.log(
+                        failedRespones[Math.floor(Math.random() * failedRespones.length)]
+                    );
+
+                    return false;
+                }
+
+                return response.json();
+            })
+            .then((data) => {
+                if (!data) return;
+                // console.log(data)
+
+                let newTypes = [];
+                data.types.forEach((e) => {
+                    newTypes.push(e.type.name);
+                });
+
+                let newStats = [];
+                data.stats.forEach((e) => {
+                    newStats.push({
+                        name: e.stat.name,
+                        base_stat: e.base_stat,
+                        effort: e.effort
+                    });
+                });
+
+                let newMoves = [];
+                data.moves.forEach((e, i) => {
+                    newMoves.push({
+                        name: e.move.name,
+                        version_group_details: e.version_group_details
+                    });
+                });
+
+                this.setState({
+                    name: data.name,
+                    sprite: data.sprites.front_default,
+                    entry: data.id,
+                    types: newTypes,
+                    stats: newStats,
+                    moves: newMoves,
+                    height: data.height,
+                    weight: data.weight,
+                    loaded: true
+                });
+                console.log(
+                    `All right! ${data.name.charAt(0).toUpperCase() +
             data.name.substr(1)} was caught!`
-        )
-      })
-  }
+                );
+            });
+    }
 
-  PokeAPIitem(item) {
-    // uncomment when incorporating items
-    // this.setState({
-    //   ...LOADING_STATE
-    // })
-    fetch(`https://pokeapi.co/api/v2/item/${item}/`)
-      .then((response) => {
-        return response.json()
-      })
-      .then((data) => {
-        this.setState({
-          name: data.name,
-          sprite: data.sprites.default,
-          entry: '',
-          types: [],
-          loaded: true
-        })
-      })
-  }
+    PokeAPIitem(item) {
+        // uncomment when incorporating items
+        // this.setState({
+        //   ...LOADING_STATE
+        // })
+        fetch(`https://pokeapi.co/api/v2/item/${item}/`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                this.setState({
+                    name: data.name,
+                    sprite: data.sprites.default,
+                    entry: '',
+                    types: [],
+                    loaded: true
+                });
+            });
+    }
 
-  render() {
-    // console.log(this.state)
-    return (
-      <div className="App" style={styles.container}>
-        {this.state.shrink ? (
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'row',
-              alignItems: 'center'
-            }}
-          >
-            <div
-              style={{
-                margin: '10px',
-                fontSize: '24px'
-              }}
-            >
-              Pokédex
+    render() {
+        // console.log(this.state)
+        return (
+            <div className="App" style={styles.container}>
+                {this.state.shrink ? (
+                    <div
+                        style={{
+                            display: 'flex',
+                            flexDirection: 'row',
+                            alignItems: 'center'
+                        }}>
+                        <div style={{ margin: '10px', fontSize: '24px' }}>
+                            Pokédex
+                        </div>
+                        <div style={{ padding: '5px' }}>
+                            <Input onInputSubmit={this.PokeAPI} shrink={this.state.shrink} />
+                        </div>
+                    </div>
+                ) : null}
+                {this.state.shrink ? null : (
+                    <div style={styles.title}>Pokédex Lookup</div>
+                )}
+
+                {this.state.shrink ? null : (
+                    <div style={styles.inputs}>
+                        <Input onInputSubmit={this.PokeAPI} />
+                    </div>
+                )}
+
+                <Pokemon
+                    name={this.state.name}
+                    number={this.state.entry}
+                    sprite={this.state.sprite}
+                    types={this.state.types}
+                    stats={this.state.stats}
+                    moves={this.state.moves}
+                    height={this.state.height}
+                    weight={this.state.weight}
+                    loaded={this.state.loaded}
+                    shrink={this.state.shrink} />
             </div>
-            <div style={{ padding: '5px' }}>
-              <Input onInputSubmit={this.PokeAPI} shrink={this.state.shrink} />
-            </div>
-          </div>
-        ) : null}
-        {this.state.shrink ? null : (
-          <div style={styles.title}>Pokédex Lookup</div>
-        )}
-
-        {this.state.shrink ? null : (
-          <div style={styles.inputs}>
-            <Input onInputSubmit={this.PokeAPI} />
-          </div>
-        )}
-
-        <Pokemon
-          name={this.state.name}
-          number={this.state.entry}
-          sprite={this.state.sprite}
-          types={this.state.types}
-          stats={this.state.stats}
-          moves={this.state.moves}
-          height={this.state.height}
-          weight={this.state.weight}
-          loaded={this.state.loaded}
-          shrink={this.state.shrink}
-        />
-      </div>
-    )
-  }
+        );
+    }
 }
 
-export default App
+export default App;
--- src/componenets/pokemon.js
+++ src/componenets/pokemon.js
@@ -1,217 +1,219 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import Card from '@material-ui/core/Card'
-import logo from '../logo.svg'
-import LaunchIcon from '@material-ui/icons/Launch'
+import Card from '@material-ui/core/Card';
+import logo from '../logo.svg';
+import LaunchIcon from '@material-ui/icons/Launch';
 
 const styles = {
-  card: {
-    margin: '20px',
-    width: '400px',
-    height: '300px',
-    display: 'flex',
-    backgroundColor: '#f0f0f0',
-    flexDirection: 'row',
-    justifyContent: 'space-evenly'
-  },
+    card: {
+        margin: '20px',
+        width: '400px',
+        height: '300px',
+        display: 'flex',
+        backgroundColor: '#f0f0f0',
+        flexDirection: 'row',
+        justifyContent: 'space-evenly'
+    },
 
-  logo: {
-    animation: 'App-logo-spin infinite 20s linear',
-    // height: '20vmin',
-    height: '100%'
-  },
+    logo: {
+        animation: 'App-logo-spin infinite 20s linear',
+        // height: '20vmin',
+        height: '100%'
+    },
 
-  left: {
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  top: {
-    width: '90%',
-    display: 'flex',
-    justifyContent: 'space-between',
-    margin: '10px'
-  },
-  sprite: {
-    margin: '-5px 0px -5px 0px'
-  },
-  bottom: {
-    height: '100%',
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    margin: '0px 0px 10px 0px'
-  },
-  // typeHeader: {
-  //   fontWeight: 'bold',
-  //   textAlign: 'center'
-  // },
-  typeList: {
+    left: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    top: {
+        width: '90%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        margin: '10px'
+    },
+    sprite: {
+        margin: '-5px 0px -5px 0px'
+    },
+    bottom: {
+        height: '100%',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        margin: '0px 0px 10px 0px'
+    },
+    // typeHeader: {
+    //   fontWeight: 'bold',
+    //   textAlign: 'center'
+    // },
+    typeList: {
     // margin: '0px 0px 10px 0px',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center'
-  },
-  type: {
-    width: '90px',
-    height: '30px',
-    color: 'white',
-    backgroundColor: '#68A090',
-    borderRadius: '40px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    },
+    type: {
+        width: '90px',
+        height: '30px',
+        color: 'white',
+        backgroundColor: '#68A090',
+        borderRadius: '40px',
 
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
 
-  right: {
-    margin: '10px 0px 10px 0px',
-    width: '100%',
-    display: 'flex',
-    flexDirection: 'column'
-  },
-  moveHeader: {
-    fontWeight: 'bold',
-    textAlign: 'center'
-  },
-  moveList: {
-    overflow: 'scroll'
-  },
-  move: {}
-}
+    right: {
+        margin: '10px 0px 10px 0px',
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    moveHeader: {
+        fontWeight: 'bold',
+        textAlign: 'center'
+    },
+    moveList: {
+        overflow: 'scroll'
+    },
+    move: {}
+};
 
 export default class Pokemon extends Component {
-  firstUpper(str) {
-    if (typeof str === 'undefined') return null
-    return str.charAt(0).toUpperCase() + str.substr(1)
-  }
+    firstUpper(str) {
+        if (typeof str === 'undefined') return null;
 
-  attachPound(str) {
-    if (isNaN(str) || str.length === 0) return str
-    return '#' + str
-  }
+        return str.charAt(0).toUpperCase() + str.substr(1);
+    }
 
-  // renderTypes() {
-  //   return <div style={styles.bottom} />
-  // }
+    attachPound(str) {
+        if (isNaN(str) || str.length === 0) return str;
 
-  render() {
-    const {
-      name,
-      number,
-      sprite,
-      types,
-      moves,
-      height,
-      weight,
-      loaded
-    } = this.props
+        return '#' + str;
+    }
 
-    return (
-      <Card
-        style={
-          this.props.shrink ? { ...styles.card, margin: '0px' } : styles.card
-        }
-      >
-        <div style={styles.left}>
-          <div style={styles.top}>
-            <div>{this.attachPound(number)} </div>
-            <div>{this.firstUpper(name)}</div>
-            <LaunchIcon
-              onClick={() => {
-                let tempURL = `https://pokeapi.co/api/v2/pokemon/${number}/`
-                if (!number) tempURL = `https://pokeapi.co/api/v2/item/${name}/`
-                window.open(tempURL, '_blank')
-              }}
-              style={{ width: '20px', height: '20px' }}
-            />
-          </div>
-          <div style={styles.sprite}>
-            {loaded ? (
-              <img
-                style={{ width: '150px', height: '150px' }}
-                src={sprite}
-                alt="pokemon sprite"
-              />
-            ) : (
-              <img
-                src={logo}
-                className="App-logo"
-                style={loaded ? { ...styles.logo, hidden: true } : styles.logo}
-                alt="react logo"
-              />
-            )}
-          </div>
-          <div style={styles.bottom}>
-            {/* <div style={styles.typeHeader}>Types</div> */}
-            <div style={styles.typeList}>
-              {types.map((e, i) => {
-                let typeColor = '#68A090'
-                if (e === 'normal') typeColor = '#A8A878'
-                else if (e === 'fire') typeColor = '#F08030'
-                else if (e === 'fighting') typeColor = '#C03028'
-                else if (e === 'water') typeColor = '#6890F0'
-                else if (e === 'flying') typeColor = '#A890F0'
-                else if (e === 'grass') typeColor = '#78C850'
-                else if (e === 'poison') typeColor = '#A040A0'
-                else if (e === 'electric') typeColor = '#F8D030'
-                else if (e === 'ground') typeColor = '#E0C068'
-                else if (e === 'psychic') typeColor = '#F85888'
-                else if (e === 'rock') typeColor = '#B8A038'
-                else if (e === 'ice') typeColor = '#98D8D8'
-                else if (e === 'bug') typeColor = '#A8B820'
-                else if (e === 'dragon') typeColor = '#7038F8'
-                else if (e === 'ghost') typeColor = '#705898'
-                else if (e === 'dark') typeColor = '#705848'
-                else if (e === 'steel') typeColor = '#B8B8D0'
-                else if (e === 'fairy') typeColor = '#EE99AC'
-                else typeColor = '#68A090'
+    // renderTypes() {
+    //   return <div style={styles.bottom} />
+    // }
 
-                return (
-                  <div
-                    style={
-                      i > 0
-                        ? {
-                            ...styles.type,
-                            marginTop: '8px',
-                            backgroundColor: typeColor
-                          }
-                        : { ...styles.type, backgroundColor: typeColor }
-                    }
-                    key={i}
-                  >
-                    {this.firstUpper(e)}
-                  </div>
-                )
-              })}
-            </div>
-            <div
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-around'
-              }}
+    render() {
+        const {
+            name,
+            number,
+            sprite,
+            types,
+            moves,
+            height,
+            weight,
+            loaded
+        } = this.props;
+
+        return (
+            <Card
+                style={
+                    this.props.shrink ? { ...styles.card, margin: '0px' } : styles.card
+                }
             >
-              <div>Height: {height}</div>
-              <div>Weight: {weight}</div>
-            </div>
-          </div>
-        </div>
-        <div style={styles.right}>
-          <div style={styles.moveHeader}>Moves</div>
-          <div style={styles.moveList}>
-            {moves.map((elem, idx) => {
-              return (
-                <div style={styles.move} key={idx}>
-                  {idx}.{this.firstUpper(elem.name)}
+                <div style={styles.left}>
+                    <div style={styles.top}>
+                        <div>{this.attachPound(number)} </div>
+                        <div>{this.firstUpper(name)}</div>
+                        <LaunchIcon
+                            onClick={() => {
+                                let tempURL = `https://pokeapi.co/api/v2/pokemon/${number}/`;
+                                if (!number) tempURL = `https://pokeapi.co/api/v2/item/${name}/`;
+                                window.open(tempURL, '_blank');
+                            }}
+                            style={{ width: '20px', height: '20px' }}
+                        />
+                    </div>
+                    <div style={styles.sprite}>
+                        {loaded ? (
+                            <img
+                                style={{ width: '150px', height: '150px' }}
+                                src={sprite}
+                                alt="pokemon sprite"
+                            />
+                        ) : (
+                            <img
+                                src={logo}
+                                className="App-logo"
+                                style={loaded ? { ...styles.logo, hidden: true } : styles.logo}
+                                alt="react logo"
+                            />
+                        )}
+                    </div>
+                    <div style={styles.bottom}>
+                        {/* <div style={styles.typeHeader}>Types</div> */}
+                        <div style={styles.typeList}>
+                            {types.map((e, i) => {
+                                let typeColor = '#68A090';
+                                if (e === 'normal') typeColor = '#A8A878';
+                                else if (e === 'fire') typeColor = '#F08030';
+                                else if (e === 'fighting') typeColor = '#C03028';
+                                else if (e === 'water') typeColor = '#6890F0';
+                                else if (e === 'flying') typeColor = '#A890F0';
+                                else if (e === 'grass') typeColor = '#78C850';
+                                else if (e === 'poison') typeColor = '#A040A0';
+                                else if (e === 'electric') typeColor = '#F8D030';
+                                else if (e === 'ground') typeColor = '#E0C068';
+                                else if (e === 'psychic') typeColor = '#F85888';
+                                else if (e === 'rock') typeColor = '#B8A038';
+                                else if (e === 'ice') typeColor = '#98D8D8';
+                                else if (e === 'bug') typeColor = '#A8B820';
+                                else if (e === 'dragon') typeColor = '#7038F8';
+                                else if (e === 'ghost') typeColor = '#705898';
+                                else if (e === 'dark') typeColor = '#705848';
+                                else if (e === 'steel') typeColor = '#B8B8D0';
+                                else if (e === 'fairy') typeColor = '#EE99AC';
+                                else typeColor = '#68A090';
+
+                                return (
+                                    <div
+                                        style={
+                                            i > 0
+                                                ? {
+                                                    ...styles.type,
+                                                    marginTop: '8px',
+                                                    backgroundColor: typeColor
+                                                }
+                                                : { ...styles.type, backgroundColor: typeColor }
+                                        }
+                                        key={i}
+                                    >
+                                        {this.firstUpper(e)}
+                                    </div>
+                                );
+                            })}
+                        </div>
+                        <div
+                            style={{
+                                display: 'flex',
+                                flexDirection: 'row',
+                                justifyContent: 'space-around'
+                            }}
+                        >
+                            <div>Height: {height}</div>
+                            <div>Weight: {weight}</div>
+                        </div>
+                    </div>
                 </div>
-              )
-            })}
-          </div>
-        </div>
-      </Card>
-    )
-  }
+                <div style={styles.right}>
+                    <div style={styles.moveHeader}>Moves</div>
+                    <div style={styles.moveList}>
+                        {moves.map((elem, idx) => {
+                            return (
+                                <div style={styles.move} key={idx}>
+                                    {idx}.{this.firstUpper(elem.name)}
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
+            </Card>
+        );
+    }
 }
